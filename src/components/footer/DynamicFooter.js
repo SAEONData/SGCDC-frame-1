60a9ee10c327711935e7938cfb1584edbd8b3908
dcugh <dianauys@gmail.com>
2019-06-26 15:46:56 +0200

    <fix frontend> handle html in modals closes #5

--- src/components/footer/DynamicFooter.js
+++ src/components/footer/DynamicFooter.js
@@ -37,41 +37,37 @@ const Err = ({ error }) => <span>Error:{error.message}</span>
 const DynamicFooter = () => {
   
   // prod server
-  const url = 'http://sgcdc.saeon.ac.za:8081/spreadsheet'
+  // const url = 'http://sgcdc.saeon.ac.za:8081/spreadsheet'
   // localhost
-  // const url = 'http://localhost:8081/spreadsheet'
+  const url = 'http://localhost:8081/spreadsheet'
   const { error, data } = useFetch(url)
   if (error) return <Err error={error} />
   if (!data) return null
  
   // const {isShowing, toggle} = useModal()
-
  
 
   // footer component populated with fetched data
   return (
-  
-
-      
- 
+   
     <MDBFooter color="blue" >
       
       <MDBContainer fluid className="text-md-center">
         
         <MDBRow >
-          <MDBCol md="3" >
+          <MDBCol md="2" >
           <ToggleContent toggle={show => 
             <h5 className="title" style={{cursor:'pointer', boxShadow:'none'}} onClick={show}>
              {data.map((data, uuid) => [uuid === 0 ? data.title : ''])}
             </h5>}
             content={hide => (
             <Modal style={{boxShadow:'none'}}>
-              <h5>
-                {data.map((data, uuid) => [uuid === 0 ? data.title : ''])}
-              </h5>
-              <p>
-                {data.map((data, uuid) => [uuid === 0 ? data.longdescription : ''])}
-              </p>
+              <h5 dangerouslySetInnerHTML =
+                { {__html: data.map((data, uuid) => [uuid === 0 ? data.title : '']).join("")}}
+              />
+              <p dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 0 ? data.longdescription : '']).join("")}}
+              />
               <button onClick={hide}>Close</button>
             </Modal>
             )}
@@ -87,12 +83,12 @@ const DynamicFooter = () => {
             </h5>}
             content={hide => (
             <Modal style={{boxShadow:'none'}}>
-              <h5>
-                {data.map((data, uuid) => [uuid === 1 ? data.title : ''])}
-              </h5>
-              <p>
-                {data.map((data, uuid) => [uuid === 1 ? data.longdescription : ''])}
-              </p>
+              <h5 dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 1 ? data.title : '']).join("")}}
+              />
+              <p dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 1 ? data.longdescription : '']).join("")}}
+              />
               <button onClick={hide}>Close</button>
             </Modal>
             )}
@@ -108,12 +104,12 @@ const DynamicFooter = () => {
             </h5>}
             content={hide => (
             <Modal style={{boxShadow:'none'}}>
-              <h5>
-                {data.map((data, uuid) => [uuid === 2 ? data.title : ''])}
-              </h5>
-              <p>
-                {data.map((data, uuid) => [uuid === 2 ? data.longdescription : ''])}
-              </p>
+              <h5 dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 2 ? data.title : '']).join("")}}
+              />
+              <p dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 2 ? data.longdescription : '']).join("")}}
+              />
               <button onClick={hide}>Close</button>
             </Modal>
             )}
@@ -129,12 +125,12 @@ const DynamicFooter = () => {
             </h5>}
             content={hide => (
             <Modal style={{boxShadow:'none'}}>
-              <h5>
-                {data.map((data, uuid) => [uuid === 3 ? data.title : ''])}
-              </h5>
-              <p>
-                {data.map((data, uuid) => [uuid === 3 ? data.longdescription : ''])}
-              </p>
+              <h5 dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 3 ? data.title : '']).join("")}}
+              />
+              <p dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 3 ? data.longdescription : '']).join("")}}
+              />
               <button onClick={hide}>Close</button>
             </Modal>
             )}
@@ -150,12 +146,12 @@ const DynamicFooter = () => {
             </h5>}
             content={hide => (
             <Modal style={{boxShadow:'none'}}>
-              <h5>
-                {data.map((data, uuid) => [uuid === 4 ? data.title : ''])}
-              </h5>
-              <p>
-                {data.map((data, uuid) => [uuid === 4 ? data.longdescription : ''])}
-              </p>
+              <h5 dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 4 ? data.title : '']).join("")}}
+              />
+              <p dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 4 ? data.longdescription : '']).join("")}}
+              />
               <button onClick={hide}>Close</button>
             </Modal>
             )}
@@ -171,12 +167,12 @@ const DynamicFooter = () => {
             </h5>}
             content={hide => (
             <Modal style={{boxShadow:'none'}}>
-              <h5>
-                {data.map((data, uuid) => [uuid === 5 ? data.title : ''])}
-              </h5>
-              <p>
-                {data.map((data, uuid) => [uuid === 5 ? data.longdescription : ''])}
-              </p>
+              <h5 dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 5 ? data.title : '']).join("")}}
+              />
+              <p dangerouslySetInnerHTML =
+                {{__html: data.map((data, uuid) => [uuid === 5 ? data.longdescription : '']).join("")}}
+              />
               <button onClick={hide}>Close</button>
             </Modal>
             )}
